1d50ceefa424569ad23900cbaf631a62f1548822
GarenWang <2270099149@qq.com>
2021-05-01 17:08:51 +0800

    feat: set header

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import './home.scss';
+import HeaderBg from '../images/header.png'
 
 export const Home = () => {
   return (
-    <div>
-      <div className="x">x</div>
+    <div className="home">
+      <div className="header" style={{backgroundImage: `url(${HeaderBg})`}}>x</div>
     </div>
   );
 };
