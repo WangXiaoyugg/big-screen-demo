56975a9f200e1f0562c230a3396d0f04d1c20a6e
GarenWang <2270099149@qq.com>
2021-05-01 17:46:16 +0800

    feat: add grid layout

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -5,7 +5,14 @@ import HeaderBg from '../images/header.png'
 export const Home = () => {
   return (
     <div className="home">
-      <div className="header" style={{backgroundImage: `url(${HeaderBg})`}}>x</div>
+      <header className="header" style={{backgroundImage: `url(${HeaderBg})`}}></header>
+      <main>
+        <section className="section1"></section>
+        <section className="section2"></section>
+        <section className="section3"></section>
+        <section className="section4"></section>
+        <section className="section5"></section>
+      </main>
     </div>
   );
 };
