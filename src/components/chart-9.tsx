2fc7abfe18717f8f8370a77b5d2507127a39432f
GarenWang <2270099149@qq.com>
2021-05-11 05:57:25 +0800

    feat: 完成chart-8

--- src/components/chart-9.tsx
+++ src/components/chart-9.tsx
@@ -8,7 +8,7 @@ import * as echarts from 'echarts';
   }, []);
 
   return (
-    <div className="age-group-3">9
+    <div className="age-group-3">
       <div ref={divRef} className="chart">
 
       </div>
